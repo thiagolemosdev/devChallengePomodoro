36028339579192d17a1356a0b54e35265de941ec
Thiago Lemos <lemoos.th@gmail.com>
2022-01-21 13:15:59 -0300

    cronometro funcional🙌🏾

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,14 +1,45 @@
 (() => {
   const tempo = document.querySelector(".tempo p");
-  const sessoes = document.querySelectorAll(".sessoes span");
+  tempo.innerText = localStorage.getItem("minutos") + ":00";
+
+  let sessoes = document.querySelectorAll(".sessoes span");
   const sessao = document.querySelector(".sessoes");
 
-  if (sessoes.length < localStorage.getItem("sessoes")) {
-    console.log(localStorage.getItem("sessoes"));
-    let span = document.createElement("span");
-    span.innerText = "o";
-    sessao.appendChild(span);
-  }
+  const comecar = document.querySelector(".play");
 
-  tempo.innerText = localStorage.getItem("minutos") + ":00";
+  function adicionaSessoes() {
+    let i = sessoes.length;
+    while (i < localStorage.getItem("sessoes")) {
+      console.log(localStorage.getItem("sessoes"));
+      let span = document.createElement("span");
+      span.innerText = "o";
+      sessao.appendChild(span);
+      i++;
+    }
+  }
+  adicionaSessoes();
+
+  let minutos = tempo.innerText.slice(0, 2);
+  let segundos = 59;
+  let contarSegundos;
+  let contarMinutos;
+
+  comecar.addEventListener("click", function () {
+    clearInterval(contarSegundos);
+    clearInterval(contarMinutos);
+    contador();
+  });
+
+  function contador() {
+    contarSegundos = setInterval(() => {
+      tempo.innerHTML = `${("00" + minutos).slice(-2)}:${(
+        "00" + segundos--
+      ).slice(-2)}`;
+    }, 1000);
+
+    contarMinutos = setInterval(() => {
+      segundos = 59;
+      minutos--;
+    }, 60000);
+  }
 })();
