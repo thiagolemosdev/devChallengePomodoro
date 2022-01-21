aa312a999e4d5e9e03b465072571a070dc950fa2
Thiago Lemos <lemoos.th@gmail.com>
2022-01-21 16:24:54 -0300

    Pausa funcional  🙌🏾

--- js/pomodoro.js
+++ js/pomodoro.js
@@ -1,6 +1,11 @@
 (() => {
   const tempo = document.querySelector(".tempo p");
   tempo.innerText = localStorage.getItem("minutos") + ":00";
+  if (tempo.innerText.length < 5) {
+    tempo.innerText = "0" + tempo.innerText;
+  }
+
+  let pausa = localStorage.getItem("pausas") - 1;
 
   let sessoes = document.querySelectorAll(".sessoes span");
   const sessao = document.querySelector(".sessoes");
@@ -10,7 +15,6 @@
   function adicionaSessoes() {
     let i = sessoes.length;
     while (i < localStorage.getItem("sessoes")) {
-      console.log(localStorage.getItem("sessoes"));
       let span = document.createElement("span");
       span.innerText = "o";
       sessao.appendChild(span);
@@ -19,7 +23,7 @@
   }
   adicionaSessoes();
 
-  let minutos = tempo.innerText.slice(0, 2);
+  let minutos = tempo.innerText.slice(0, -3) - 1;
   let segundos = 59;
   let contarSegundos;
   let contarMinutos;
@@ -30,11 +34,35 @@
     contador();
   });
 
+  function intervalo() {
+    console.log;
+    contarSegundos = setInterval(() => {
+      tempo.innerHTML = `${("00" + pausa).slice(-2)}:${(
+        "00" + segundos--
+      ).slice(-2)}`;
+    }, 1000);
+
+    contarMinutos = setInterval(() => {
+      segundos = 59;
+      pausa--;
+    }, 60000);
+  }
+
+  function finalTrabalho() {
+    if (segundos == 0 && minutos == 0) {
+      clearInterval(contarSegundos);
+      clearInterval(contarMinutos);
+      intervalo();
+      segundos = 59;
+    }
+  }
+
   function contador() {
     contarSegundos = setInterval(() => {
       tempo.innerHTML = `${("00" + minutos).slice(-2)}:${(
         "00" + segundos--
       ).slice(-2)}`;
+      finalTrabalho();
     }, 1000);
 
     contarMinutos = setInterval(() => {
