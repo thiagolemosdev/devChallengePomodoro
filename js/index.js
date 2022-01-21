f9da3bf341c1c64e69fa509d5a813d8b2929728c
Thiago Lemos <lemoos.th@gmail.com>
2022-01-21 02:25:57 -0300

    capturando os dados

---
+++ js/index.js
@@ -0,0 +1,36 @@
+function pegarDados() {
+  const botoes = document.querySelectorAll(".seta");
+  let minutos = document.querySelector(".minutos");
+  let pausas = document.querySelector(".pausas");
+  let sessoes = document.querySelector(".sessoes");
+
+  botoes.forEach((el) => {
+    el.addEventListener("click", function (event) {
+      if (event.target.classList.contains("aumenta-minutos")) {
+        const num = +minutos.innerText + 1;
+        minutos.innerText = num;
+      } else if (event.target.classList.contains("diminui-minutos")) {
+        const num = +minutos.innerText - 1;
+        minutos.innerText = num;
+      } else if (event.target.classList.contains("aumenta-pausa")) {
+        const num = +pausas.innerText + 1;
+        pausas.innerText = num;
+      } else if (event.target.classList.contains("diminui-pausa")) {
+        const num = +pausas.innerText - 1;
+        pausas.innerText = num;
+      } else if (event.target.classList.contains("aumenta-sessoes")) {
+        const num = +sessoes.innerText + 1;
+        sessoes.innerText = num;
+      } else if (event.target.classList.contains("diminui-sessoes")) {
+        const num = +sessoes.innerText - 1;
+        sessoes.innerText = num;
+      }
+    });
+  });
+
+  const comecar = document.querySelector(".comecar");
+  comecar.addEventListener("click", function (event) {
+    event.preventDefault();
+  });
+}
+pegarDados();
